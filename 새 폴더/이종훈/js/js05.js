5c17cab65eae7229d0e24b70eeac4768d50f5e7b
mcluru <wrose46@naver.com>
2023-03-13 19:39:56 +0900

    230313

--- 새 폴더/이종훈/js/js05.js
+++
@@ -1,8 +0,0 @@
-//li querySelectorALL: 배열로 리턴, removeChild()
-const li_lists= document.querySelector('li'); //모든 li태그 선택
-
-for(let li of li_lists) {
-  li.addEventListener('click', function() {
-    this.parentNode.removeChild(this);
-  })
-}
