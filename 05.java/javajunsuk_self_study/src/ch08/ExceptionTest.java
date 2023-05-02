9a5a9433b8bef67524001fa3c627e8bd7a1fc236
mcluru <wrose46@naver.com>
2023-05-02 19:54:44 +0900

    230502

--- 05.java/javajunsuk_self_study/src/ch08/ExceptionTest.java
+++ 05.java/javajunsuk_self_study/src/ch08/ExceptionTest.java
@@ -4,7 +4,7 @@ public class ExceptionTest {
 
 	public static void main(String[] args) {
 		
-		system.out.println(args[0]);	//컴파일에러 발생
+//		system.out.println(args[0]);	//컴파일에러 발생
 		System.out.println(args[0]);	//런타임에러 발생
 		
 	}
