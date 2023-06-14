d26266b5770feef0c75a0f8db47f4dea6e60a3b6
mcluru <wrose46@naver.com>
2023-06-14 13:11:27 +0900

    20230614-2

---
+++ 08.react/lec01_basic/src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = (props) => {
+  const {title} = props;
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+};
+
+export default Title;
---
+++ 08.react/lec01_basic/src/components/TodoList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const TodoList = ({props}) => {
+
+  const {todos} = props;  //data(title, todos)
+
+  console.log(`${todos}`);
+  var todolist = {};
+  // var todolist = todos.map((item, index) => {
+  //   let status = ''
+  //   if(item.done === true) status = '완료'
+  //   else status = '진행중'
+  //   return (<li key={item.id}>{item.todo} : {status}</li>)
+  // });
+
+  return (
+    <div>
+      <ul>{todolist}</ul>
+    </div>
+  );
+};
+
+export default TodoList;
